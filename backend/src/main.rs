eaf201e634ae27bc8d2922c868854f88458d37fa
FabianLars <fabianlars@fabianlars.de>
2020-05-07 22:06:23 +0200

    commited old changes; moved frontend to _old; yew template init

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,8 @@
-use serde::{Deserialize, Serialize};
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let get_champion_file = warp::path!("wapi" / "champion").and(warp::fs::file("./champions.json"));
+    let get_champion_file = warp::path!("wiki" / "champion").and(warp::fs::file("./champions.json"));
 
     warp::serve(get_champion_file)
         .run(([127, 0, 0, 1], 3030))
