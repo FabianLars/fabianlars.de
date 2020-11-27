3f25c381c961a51a2e60c7160a0bc38b6434eab4
FabianLars <fabianlars@fabianlars.de>
2020-11-27 22:21:01 +0100

    fmt

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,7 +2,8 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let get_champion_file = warp::path!("wiki" / "champion").and(warp::fs::file("./champions.json"));
+    let get_champion_file =
+        warp::path!("wiki" / "champion").and(warp::fs::file("./champions.json"));
 
     warp::serve(get_champion_file)
         .run(([127, 0, 0, 1], 3030))
