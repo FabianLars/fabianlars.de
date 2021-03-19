b674dae2a2d9b01c5e526085bd4e650a94e8c749
FabianLars <fabianlars@fabianlars.de>
2021-03-19 17:54:47 +0100

    fix errors

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,14 @@
 use chrono::NaiveDate;
 use serde::Serialize;
-use sqlx::postgres::PgPoolOptions;
-use warp::{Filter, Rejection};
+use sqlx::postgres::{PgPool, PgPoolOptions};
+use warp::{reply::Json, Filter, Rejection};
 
 #[derive(Serialize)]
 struct Rotation {
-    rotation_id: u16,
+    rotation_id: i32,
     start_date: NaiveDate,
     end_date: NaiveDate,
-    rotation: Vec<String>,
+    champions: Vec<String>,
 }
 
 #[tokio::main]
@@ -20,37 +20,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .connect(&std::env::var("DATABASE_URL")?)
         .await?;
 
-    let rotations = warp::path!("lol" / "rotations").and_then(|| async {
-        let rows = sqlx::query_as!(Rotation, "SELECT * FROM rotations")
-            .fetch_all(&pool)
-            .await?;
+    let rotations = warp::path!("lol" / "rotations")
+        .and(with_db(pool.clone()))
+        .and_then(get_rotations);
 
-        Ok(rows)
-    });
+    let rotation = warp::path!("lol" / "rotations" / u16)
+        .and(with_db(pool.clone()))
+        .and_then(get_rotation);
 
-    let rotation = warp::path!("lol" / "rotations" / u16).and_then(|id: u16| async {
-        let row = sqlx::query_as!(
-            Rotation,
-            "SELECT * FROM rotations WHERE rotation_id = ?",
-            id,
-        )
-        .fetch_one(&pool)
-        .await?;
-
-        Ok(row)
-    });
-
-    let rotation_latest = warp::path!("lol" / "rotations" / "latest").and_then(|| async {
-        let row = sqlx::query_as!(
-            Rotation,
-            "SELECT * FROM rotations ORDER BY rotation_id DESC LIMIT 1",
-            id,
-        )
-        .fetch_one(&pool)
-        .await?;
-
-        Ok(row)
-    });
+    let rotation_latest = warp::path!("lol" / "rotations" / "latest")
+        .and(with_db(pool.clone()))
+        .and_then(get_rotation_latest);
 
     let champions = warp::path!("lol" / "champions").and(warp::fs::file("./champions.json"));
 
@@ -76,6 +56,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+fn with_db(
+    pool: PgPool,
+) -> impl Filter<Extract = (PgPool,), Error = std::convert::Infallible> + Clone {
+    warp::any().map(move || pool.clone())
+}
+
 async fn get_champion(id: u16) -> Result<String, Rejection> {
     let file = tokio::fs::read_to_string("./champions.json")
         .await
@@ -88,4 +74,38 @@ async fn get_champion(id: u16) -> Result<String, Rejection> {
     } else {
         Err(warp::reject::not_found())
     }
+}
+
+async fn get_rotations(pool: PgPool) -> Result<Json, Rejection> {
+    let rows = sqlx::query_as!(Rotation, "SELECT * FROM rotations")
+        .fetch_all(&pool)
+        .await
+        .map_err(|_| warp::reject::not_found())?;
+
+    Ok(warp::reply::json(&rows))
+}
+
+async fn get_rotation(id: u16, pool: PgPool) -> Result<Json, Rejection> {
+    let row = sqlx::query_as!(
+        Rotation,
+        "SELECT * FROM rotations WHERE rotation_id = $1",
+        id as i32,
+    )
+    .fetch_one(&pool)
+    .await
+    .map_err(|_| warp::reject::not_found())?;
+
+    Ok(warp::reply::json(&row))
+}
+
+async fn get_rotation_latest(pool: PgPool) -> Result<Json, Rejection> {
+    let row = sqlx::query_as!(
+        Rotation,
+        "SELECT * FROM rotations ORDER BY rotation_id DESC LIMIT 1"
+    )
+    .fetch_one(&pool)
+    .await
+    .map_err(|_| warp::reject::not_found())?;
+
+    Ok(warp::reply::json(&row))
 }
